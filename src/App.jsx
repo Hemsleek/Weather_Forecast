5de7f3b263a382a2af08b21ae139e45cd6c783c2
hemsleek <asiyanbimubashir@gmail.com>
2021-04-11 15:37:53 +0100

    feat: project structure, main and Additional info section

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,43 @@ const MainSection = () => {
 
         <div className="searchBar">
           <span>Your City</span>
-          <input type="text" placeholder='Nigeria'/>
+          <input type="text" placeholder='Enter City Name'/>
+        </div>
+        
+        <div className="main">
+          <span className="time">12.05 PM, Sun, April 11, 2021 </span>
+
+          <div className="weather-condition">
+
+            <div className="cloud-info">
+              <img src="./imgs/cloud.svg" alt="cloud-icon"/>
+              <span>72<sup>o<sub>F</sub></sup> </span>
+            </div>
+            <span>Cloudy</span>
+          </div>
+
+          <div className="info">
+            <div className="humidity info-item">
+              <span>Humidity</span>
+              <span>45%</span>
+            </div>
+            <div className="wind-speed info-item">
+              <span>Wind Speed</span>
+              <span>19.2 km/j</span>
+            </div>
+          </div>
         </div>
 
-        <span>12.05 PM, Sun, April 11, 2021 </span>
-
-        <div className="weather-condition">
-
-          <div className="cloud-info">
-            <img src="./imgs/cloud.svg" alt="cloud-icon"/>
-            <span>72 <sup>oF</sup> </span>
-          </div>
-          <span>Cloudy</span>
-        </div>
-
-        <div className="info">
-          <div className="humidity">
-            <span>Humidity</span>
-            <span>45%</span>
-          </div>
-          <div className="wind-speed">
-            <span>Wind Speed</span>
-            <span>19.2 km/j</span>
-          </div>
-        </div>
       </section>
   )
 }
+
 const AdditionalInfo = () => {
   return(
     <section className="AdditionalInfo">
       <div className="chart-wrapper">
         <span>Temperature</span>
-        <div className="chart"></div>
+        <div className="chart"> chart</div>
       </div>
       <div className="extra-info">
         {
@@ -57,7 +61,10 @@ const AdditionalInfo = () => {
 function App() {
   return (
     <div className="App">
-      loaded
+      <div className="wrapper">
+        <MainSection />
+        <AdditionalInfo />
+      </div>
     </div>
   );
 }
